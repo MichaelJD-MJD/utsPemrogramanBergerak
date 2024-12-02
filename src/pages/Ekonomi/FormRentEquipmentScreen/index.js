1ee3a65ccfeaf3ee58e3ec57b4f71d3c00949c7e
zahwaap <zahwa1220@gmail.com>
2024-12-02 11:43:53 +0700

    formrentilkom

--- src/pages/Ekonomi/FormRentEquipmentScreen/index.js
+++ src/pages/Ekonomi/FormRentEquipmentScreen/index.js
@@ -37,7 +37,7 @@ const FormRentEquipmentScreen = ({ navigation }) => {
       {/* Form Info */}
       <View style={styles.formInfo}>
         <Text style={styles.formTitle}>Formulir Peminjaman</Text>
-        <Text style={styles.faculty}>Fakultas Ilmu Komputer</Text>
+        <Text style={styles.faculty}>Fakultas Ekonomi</Text>
       </View>
 
       {/* Image */}
